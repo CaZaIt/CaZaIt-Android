6443e909ec79e5303033654e06a1c1cf10728c7b
Lee Gilho <dlrlfgh517@gmail.com>
2023-01-12 18:35:23 +0900

    [Add] #2 - login activity xml

---
+++ app/src/main/java/org/cazait/cazait_android/ui/view/login/LoginActivity.kt
@@ -0,0 +1,28 @@
+package org.cazait.cazait_android.ui.view.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.cazait.cazait_android.R
+import org.cazait.cazait_android.databinding.ActivityLoginBinding
+import org.cazait.cazait_android.ui.view.MainActivity
+
+class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_login
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding.textView7.setOnClickListener {
+            val next = Intent(this, MainActivity::class.java)
+            startActivity(next)
+        }
+    }
+}
