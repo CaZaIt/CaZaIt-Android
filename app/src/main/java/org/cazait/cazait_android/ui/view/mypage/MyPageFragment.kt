77b22a2a710887041199ea0e6cd6fc54db56b52b
yibeomseok <vv99911@gmail.com>
2023-01-17 11:20:07 +0900

    [Add] my page fragment

--- app/src/main/java/org/cazait/cazait_android/ui/view/mypage/MyPageFragment.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/mypage/MyPageFragment.kt
@@ -1,10 +1,5 @@
 package org.cazait.cazait_android.ui.view.mypage
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import org.cazait.cazait_android.R
 import org.cazait.cazait_android.databinding.FragmentMyPageBinding
