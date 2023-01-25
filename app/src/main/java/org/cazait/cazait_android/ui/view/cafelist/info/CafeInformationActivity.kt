4d9f0c6d36a6c66800745c958a8fdd9908017aae
wbham97 <wbham97@naver.com>
2023-01-25 13:18:01 +0900

    [Chore] #18 - CafeInformationActivity 코드 추가
    - 새로운 구조에 코드 추가

--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/CafeInformationActivity.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/CafeInformationActivity.kt
@@ -1,14 +1,22 @@
 package org.cazait.cazait_android.ui.view.cafelist.info
 
+import android.widget.Button
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import org.cazait.cazait_android.R
 import org.cazait.cazait_android.databinding.ActivityCafeInformationBinding
 import org.cazait.cazait_android.ui.base.BaseActivity
+import org.cazait.cazait_android.ui.view.cafelist.info.inner.CafeLocTransFragment
+import org.cazait.cazait_android.ui.view.cafelist.info.inner.CafeMenuFragment
+import org.cazait.cazait_android.ui.view.cafelist.info.inner.CafeRatingReviewFragment
+import org.cazait.cazait_android.ui.view.cafelist.info.inner.OnMapTouchListener
 import org.cazait.cazait_android.ui.viewmodel.CafeInfoViewModel
 
 @AndroidEntryPoint
-class CafeInformationActivity : BaseActivity<ActivityCafeInformationBinding, CafeInfoViewModel>() {
+class CafeInformationActivity : BaseActivity<ActivityCafeInformationBinding, CafeInfoViewModel>(),
+    OnMapTouchListener {
     override val layoutResourceId: Int
         get() = R.layout.activity_cafe_information
 
@@ -22,6 +30,83 @@ class CafeInformationActivity : BaseActivity<ActivityCafeInformationBinding, Caf
     }
 
     override fun initView() {
+        val toolbar = binding.toolbar
+        toolbar.title = "롬곡"
+        binding.tvCafeInfoAdd.text = "서울특별시 광진구 광나루로 375-1 2층(군자동)"
+        setSupportActionBar(toolbar)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+        binding.fabReviewWrite.hide()
 
+        binding.btnCafeMenu.setSelected(true)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.cafe_info_frag_con, CafeMenuFragment())
+            .commit()
+
+        setFragment1(
+            binding.btnCafeMenu,
+            binding.btnCafeLocTrans,
+            binding.btnCafeRatingReview,
+            CafeMenuFragment(),
+            binding.fabReviewWrite
+        )
+        setFragment1(
+            binding.btnCafeLocTrans,
+            binding.btnCafeRatingReview,
+            binding.btnCafeMenu,
+            CafeLocTransFragment(),
+            binding.fabReviewWrite
+        )
+        setFragment2(
+            binding.btnCafeRatingReview,
+            binding.btnCafeMenu,
+            binding.btnCafeLocTrans,
+            CafeRatingReviewFragment(),
+            binding.fabReviewWrite
+        )
+    }
+
+    private fun setFragment1(
+        btn1: Button,
+        btn2: Button,
+        btn3: Button,
+        frag: Fragment,
+        fab: FloatingActionButton
+    ) {
+        btn1.setOnClickListener {
+            btn1.setSelected(true)
+            btn2.setSelected(false)
+            btn3.setSelected(false)
+            supportFragmentManager.popBackStack()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.cafe_info_frag_con, frag)
+                .commit()
+            fab.hide()
+        }
+    }
+
+    private fun setFragment2(
+        btn1: Button,
+        btn2: Button,
+        btn3: Button,
+        frag: Fragment,
+        fab: FloatingActionButton
+    ) {
+        btn1.setOnClickListener {
+            btn1.setSelected(true)
+            btn2.setSelected(false)
+            btn3.setSelected(false)
+            supportFragmentManager.popBackStack()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.cafe_info_frag_con, frag)
+                .commit()
+            fab.show()
+        }
+    }
+
+    override fun onTouch() {
+        binding.cafeInfoFragCon.requestDisallowInterceptTouchEvent(true)
     }
 }
