aeed90c4a04055f8476e38bcb49653fcc4110478
wbham97 <wbham97@naver.com>
2023-01-27 15:06:00 +0900

    [Feat] #29 - 리뷰 작성하는 페이지 추가
    - 리뷰 작성하는 페이지 추가
    - 별점 메길때 일일히 이미지로 넣어줌
    - 이미지를 눌렀을 때 색이 바뀌는 것을 코드로 구현
    - RatingBar라는 라이브러리 존재
    - 현재는 별점 1점단위

--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/CafeInformationActivity.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/CafeInformationActivity.kt
@@ -1,5 +1,6 @@
 package org.cazait.cazait_android.ui.view.cafelist.info
 
+import android.content.Intent
 import android.widget.Button
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
@@ -8,10 +9,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import org.cazait.cazait_android.R
 import org.cazait.cazait_android.databinding.ActivityCafeInformationBinding
 import org.cazait.cazait_android.ui.base.BaseActivity
-import org.cazait.cazait_android.ui.view.cafelist.info.inner.CafeLocTransFragment
-import org.cazait.cazait_android.ui.view.cafelist.info.inner.CafeMenuFragment
-import org.cazait.cazait_android.ui.view.cafelist.info.inner.CafeRatingReviewFragment
-import org.cazait.cazait_android.ui.view.cafelist.info.inner.OnMapTouchListener
+import org.cazait.cazait_android.ui.view.cafelist.info.inner.*
 import org.cazait.cazait_android.ui.viewmodel.CafeInfoViewModel
 
 @AndroidEntryPoint
@@ -67,6 +65,11 @@ class CafeInformationActivity : BaseActivity<ActivityCafeInformationBinding, Caf
             CafeRatingReviewFragment(),
             binding.fabReviewWrite
         )
+
+        binding.fabReviewWrite.setOnClickListener {
+            val intent=Intent(this,CafeRatingReviewEdit::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun setFragment1(
--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewEdit.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewEdit.kt
@@ -1,12 +1,94 @@
 package org.cazait.cazait_android.ui.view.cafelist.info.inner
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import org.cazait.cazait_android.R
+import org.cazait.cazait_android.databinding.ActivityCafeRatingReviewEditBinding
+import org.cazait.cazait_android.ui.base.BaseActivity
+import org.cazait.cazait_android.ui.viewmodel.CafeInfoReviewViewModel
 
-class CafeRatingReviewEdit : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cafe_rating_review_edit)
+class CafeRatingReviewEdit :
+    BaseActivity<ActivityCafeRatingReviewEditBinding, CafeInfoReviewViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_cafe_rating_review_edit
+    override val viewModel: CafeInfoReviewViewModel by viewModels()
+
+    override fun initBeforeBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+    @SuppressLint("ResourceAsColor")
+    override fun initView() {
+        var imageView1Clicked: Boolean = false
+        var imageView2Clicked: Boolean = false
+        var imageView3Clicked: Boolean = false
+        var imageView4Clicked: Boolean = false
+        var imageView5Clicked: Boolean = false
+//        binding.imgvStarPress1.setOnClickListener {
+//            when (imageView1Clicked) {
+//                false -> {
+//                    binding.imgvStarPress1.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
+//                    imageView1Clicked = !imageView1Clicked
+//                }
+//                true -> {
+//                    binding.imgvStarPress1.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
+//                    imageView1Clicked = !imageView1Clicked
+//                }
+//            }
+//        }
+//        binding.imgvStarPress2.setOnClickListener {
+//            when(imageView2Clicked){
+//                false ->{
+//                    binding.imgvStarPress2.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
+//                    imageView2Clicked = !imageView2Clicked
+//                }
+//                true -> {
+//                    binding.imgvStarPress2.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
+//                    imageView2Clicked = !imageView2Clicked
+//                }
+//            }
+//        }
+//        binding.imgvStarPress3.setOnClickListener {
+//            when(imageView3Clicked){
+//                false ->{
+//                    binding.imgvStarPress3.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
+//                    imageView3Clicked = !imageView3Clicked
+//                }
+//                true -> {
+//                    binding.imgvStarPress3.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
+//                    imageView3Clicked = !imageView3Clicked
+//                }
+//            }
+//        }
+//        binding.imgvStarPress4.setOnClickListener {
+//            when(imageView4Clicked){
+//                false ->{
+//                    binding.imgvStarPress4.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
+//                    imageView4Clicked = !imageView4Clicked
+//                }
+//                true -> {
+//                    binding.imgvStarPress4.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
+//                    imageView4Clicked = !imageView4Clicked
+//                }
+//            }
+//        }
+//        binding.imgvStarPress5.setOnClickListener {
+//            when(imageView5Clicked){
+//                false ->{
+//                    binding.imgvStarPress5.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
+//                    imageView5Clicked = !imageView5Clicked
+//                }
+//                true -> {
+//                    binding.imgvStarPress5.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
+//                    imageView5Clicked = !imageView5Clicked
+//                }
+//            }
+//        }
+
     }
 }
