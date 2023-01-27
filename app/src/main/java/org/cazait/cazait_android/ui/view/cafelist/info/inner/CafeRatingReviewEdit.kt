9c66d9eff9b453ac02a3e735ecf0560258de3dc0
wbham97 <wbham97@naver.com>
2023-01-27 15:46:23 +0900

    [Feat] #28 - RatingBar를 통해 별점 주기
    - RatingBar에서 사용자가 입력한 별점을 Log로 찍기
    - 작성하기 버튼을 누르면 사용자가 입력한 후기랑 별점이 서버로 넘어감
    - 그리고 Edit Activity는 완전히 종료

--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewEdit.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewEdit.kt
@@ -1,6 +1,7 @@
 package org.cazait.cazait_android.ui.view.cafelist.info.inner
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import androidx.activity.viewModels
 import org.cazait.cazait_android.R
 import org.cazait.cazait_android.databinding.ActivityCafeRatingReviewEditBinding
 import org.cazait.cazait_android.ui.base.BaseActivity
+import org.cazait.cazait_android.ui.view.cafelist.info.CafeInformationActivity
 import org.cazait.cazait_android.ui.viewmodel.CafeInfoReviewViewModel
 
 class CafeRatingReviewEdit :
@@ -23,6 +25,14 @@ class CafeRatingReviewEdit :
     }
 
     override fun initView() {
-
+        binding.ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            Log.d("RatingBar Score","{$rating}점")
+        }
+        binding.btnReviewOk.setOnClickListener {
+            val intent = Intent(this, CafeInformationActivity::class.java)
+            Log.d("RatingBar Score","{${binding.ratingBar.rating}}점")
+            startActivity(intent)
+            finish()
+        }
     }
 }
