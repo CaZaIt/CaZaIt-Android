36f2d8d9d00241cc9c8a0bcfbb7ab8aa8946e36d
wbham97 <wbham97@naver.com>
2023-01-27 15:31:28 +0900

    [Feat] #28 - RatingBar 추가
    - 기존의 별을 일일히 하나씩 넣기보다 RatingBar를 이용하여 별점을 좀 더
      편리하게 메길 수 있도록 구현
    - 단순하게 RatingBar만 넣었고 추가적인 기능은 추후에 넣을 예정

--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewEdit.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewEdit.kt
@@ -22,73 +22,7 @@ class CafeRatingReviewEdit :
     override fun initAfterBinding() {
     }
 
-    @SuppressLint("ResourceAsColor")
     override fun initView() {
-        var imageView1Clicked: Boolean = false
-        var imageView2Clicked: Boolean = false
-        var imageView3Clicked: Boolean = false
-        var imageView4Clicked: Boolean = false
-        var imageView5Clicked: Boolean = false
-//        binding.imgvStarPress1.setOnClickListener {
-//            when (imageView1Clicked) {
-//                false -> {
-//                    binding.imgvStarPress1.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
-//                    imageView1Clicked = !imageView1Clicked
-//                }
-//                true -> {
-//                    binding.imgvStarPress1.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
-//                    imageView1Clicked = !imageView1Clicked
-//                }
-//            }
-//        }
-//        binding.imgvStarPress2.setOnClickListener {
-//            when(imageView2Clicked){
-//                false ->{
-//                    binding.imgvStarPress2.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
-//                    imageView2Clicked = !imageView2Clicked
-//                }
-//                true -> {
-//                    binding.imgvStarPress2.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
-//                    imageView2Clicked = !imageView2Clicked
-//                }
-//            }
-//        }
-//        binding.imgvStarPress3.setOnClickListener {
-//            when(imageView3Clicked){
-//                false ->{
-//                    binding.imgvStarPress3.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
-//                    imageView3Clicked = !imageView3Clicked
-//                }
-//                true -> {
-//                    binding.imgvStarPress3.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
-//                    imageView3Clicked = !imageView3Clicked
-//                }
-//            }
-//        }
-//        binding.imgvStarPress4.setOnClickListener {
-//            when(imageView4Clicked){
-//                false ->{
-//                    binding.imgvStarPress4.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
-//                    imageView4Clicked = !imageView4Clicked
-//                }
-//                true -> {
-//                    binding.imgvStarPress4.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
-//                    imageView4Clicked = !imageView4Clicked
-//                }
-//            }
-//        }
-//        binding.imgvStarPress5.setOnClickListener {
-//            when(imageView5Clicked){
-//                false ->{
-//                    binding.imgvStarPress5.setImageResource(R.drawable.cafe_info_rating_review_edit_star_full)
-//                    imageView5Clicked = !imageView5Clicked
-//                }
-//                true -> {
-//                    binding.imgvStarPress5.setImageResource(R.drawable.cafe_info_rating_review_edit_star_empty)
-//                    imageView5Clicked = !imageView5Clicked
-//                }
-//            }
-//        }
 
     }
 }
