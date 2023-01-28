d79cb22ba71a06c340c3e9f9bc7c357b5bf1fa97
wbham97 <wbham97@naver.com>
2023-01-28 17:28:25 +0900

    [Feat] #37 - Review 정렬 기준 선택 Dialog
    - RatingReviewFragment에서 '최근 작성순'과 화살표를 누르면 정렬 기준을
      선택할 수 있는 Dialog 생성
    - 추후에 삭제될 수도 있음
    - res->values->string에 string배열 하나 추가
    - CafeRatingReviewFragment 하단에 showDialog 함수 및 setOnClickListener
      추가
    - 그 외에 변경사항 X

--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewFragment.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/CafeRatingReviewFragment.kt
@@ -1,5 +1,7 @@
 package org.cazait.cazait_android.ui.view.cafelist.info.inner
 
+import android.app.AlertDialog
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -33,5 +35,24 @@ class CafeRatingReviewFragment :
         binding.cafeInfoReviewListView.layoutManager =
             LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         binding.cafeInfoReviewListView.adapter = reviewRVAdapter
+
+        binding.tvFilter.setOnClickListener {
+            showDialog()
+        }
+        binding.imgbtnFilter.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    // 정렬 기준 선택 다이얼로그
+    private fun showDialog(){
+        val sortMenu:Array<String> = resources.getStringArray(R.array.sortmenu)
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder.setTitle("정렬 메뉴")
+        builder.setItems(sortMenu){ p0, p1 ->
+            Toast.makeText(context, "선택된 정렬 기준은 ${sortMenu[p1]}", Toast.LENGTH_LONG).show()
+        }
+        val alertDialog:AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
