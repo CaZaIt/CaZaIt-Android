1eccdfb52a9e83963fc6d73908153c6987c51167
bmsk <vv99911@gmail.com>
2023-01-30 16:55:50 +0900

    [Add] #33 - update base activity - dispatchTouchEvent

--- app/src/main/java/org/cazait/cazait_android/ui/base/BaseActivity.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/base/BaseActivity.kt
@@ -1,6 +1,10 @@
 package org.cazait.cazait_android.ui.base
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -26,6 +30,26 @@ abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatA
      * initiate others (ex. observe Livedata)
      */
     abstract fun initAfterBinding()
+
+    /**
+     * 키보드 위 빈 공간을 터치하면 키보드가 사라지도록 한다
+     */
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val focusView: View? = currentFocus
+        if (focusView != null) {
+            val rect = Rect()
+            focusView.getGlobalVisibleRect(rect)
+            val x = ev.x.toInt()
+            val y = ev.y.toInt()
+            if (!rect.contains(x, y)) {
+                val imm: InputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(focusView.windowToken, 0)
+                focusView.clearFocus()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
