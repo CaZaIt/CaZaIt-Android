dfa1257453354771bd6d2f6aed50de87304f77d8
wbham97 <wbham97@naver.com>
2023-01-31 17:32:28 +0900

    [Add] CafeInterestViewModel id 번호 추가

--- app/src/main/java/org/cazait/cazait_android/CafeInterestViewModel.kt
+++ app/src/main/java/org/cazait/cazait_android/CafeInterestViewModel.kt
@@ -24,19 +24,19 @@ open class CafeInterestViewModel @Inject constructor(private val dataRepository:
 
     private fun initDataSet() {
         val dataList = arrayListOf(
-            CafeState("카페 범쿤","100", "서울시 광진구 능동 239 - 26", "보통"),
-            CafeState("눈물","200", "서울시 광진구 xxxx - xxxxxx", "혼잡"),
-            CafeState("눙물","300", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("국물","400", "서울시 광진구 xxxx - xxxxxx", "여유"),
-            CafeState("물논","1200", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("물론","400", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("스타벅스 광진 능동점","500", "서울시 광진구 xxxx - xxxxxx", "혼잡"),
-            CafeState("골목","600", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("눔룬","700", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("엔제리너스카페24시","800", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("제주몰빵","900", "서울시 광진구 xxxx - xxxxxx", "보통"),
-            CafeState("카페 딕셔너리","1000", "서울시 xxxx - xxxxxx", "보통"),
-            CafeState("카페베네","1100", "서울시 광진구 xxxx - xxxxxx", "보통")
+            CafeState("1","카페 범쿤","100", "서울시 광진구 능동 239 - 26", "보통"),
+            CafeState("2","눈물","200", "서울시 광진구 xxxx - xxxxxx", "혼잡"),
+            CafeState("3","눙물","300", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("4","국물","400", "서울시 광진구 xxxx - xxxxxx", "여유"),
+            CafeState("5","물논","1200", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("6","물론","400", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("7","스타벅스 광진 능동점","500", "서울시 광진구 xxxx - xxxxxx", "혼잡"),
+            CafeState("8","골목","600", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("9","눔룬","700", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("10","엔제리너스카페24시","800", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("11","제주몰빵","900", "서울시 광진구 xxxx - xxxxxx", "보통"),
+            CafeState("12","카페 딕셔너리","1000", "서울시 xxxx - xxxxxx", "보통"),
+            CafeState("13","카페베네","1100", "서울시 광진구 xxxx - xxxxxx", "보통")
 
         )
         setCafeinterestList(dataList)
