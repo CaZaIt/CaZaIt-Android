e1c3bd1da7e8055faedd4424179fbe64e955cfcf
wbham97 <wbham97@naver.com>
2023-02-01 16:36:19 +0900

    [Chore] #60 - 안쓰는 라이브러리 삭제

--- app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/ImageSlideFragment.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/view/cafelist/info/inner/ImageSlideFragment.kt
@@ -1,10 +1,5 @@
 package org.cazait.cazait_android.ui.view.cafelist.info.inner
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.cazait.cazait_android.R
