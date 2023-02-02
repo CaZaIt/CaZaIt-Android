ed88d483292399b94a0d3bcd9a53aeae0db57c89
bmsk <vv99911@gmail.com>
2023-02-02 16:04:51 +0900

    [Fix] viewmodel

--- app/src/main/java/org/cazait/cazait_android/ui/viewmodel/CafeInfoImgViewModel.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/viewmodel/CafeInfoImgViewModel.kt
@@ -1,11 +1,11 @@
 package org.cazait.cazait_android.ui.viewmodel
 
 import dagger.hilt.android.lifecycle.HiltViewModel
-import org.cazait.cazait_android.data.repository.DataRepositorySource
+import org.cazait.cazait_android.data.repository.DataRepository
 import org.cazait.cazait_android.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CafeInfoImgViewModel @Inject constructor(private val dataRepository: DataRepositorySource) :
+class CafeInfoImgViewModel @Inject constructor(private val dataRepository: DataRepository) :
     BaseViewModel() {
 }
