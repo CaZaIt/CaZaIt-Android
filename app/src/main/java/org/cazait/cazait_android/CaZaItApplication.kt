db076754ec718fc45127c19e6edd8660de1ba34b
bmsk <vv99911@gmail.com>
2023-02-02 18:08:13 +0900

    [Refactor] #70 - BottomNavigation

--- app/src/main/java/org/cazait/cazait_android/CaZaItApplication.kt
+++ app/src/main/java/org/cazait/cazait_android/CaZaItApplication.kt
@@ -1,7 +1,13 @@
 package org.cazait.cazait_android
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CaZaItApplication: Application()
+class CaZaItApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
