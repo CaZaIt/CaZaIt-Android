aaf304fe4bb88a130c006e6bec41829ca6450334
bmsk <vv99911@gmail.com>
2023-02-03 01:22:17 +0900

    [Refactor] #74 - API name
    API를 Service로 변경

--- app/src/main/java/org/cazait/cazait_android/data/api/CafeService.kt
+++ app/src/main/java/org/cazait/cazait_android/data/api/CafeService.kt
@@ -1,10 +1,9 @@
 package org.cazait.cazait_android.data.api
 
 import org.cazait.cazait_android.data.dto.cafe.CafesDto
-import retrofit2.Call
 import retrofit2.http.*
 
-interface CafeAPI {
+interface CafeService {
     /**
      * 근처 카페들에 대한 정보를 불러온다.
      */
