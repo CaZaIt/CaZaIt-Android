c9c7cc36c9f5eacdcd8fa65d79028be9b4c33eff
bmsk <vv99911@gmail.com>
2023-02-06 13:22:18 +0900

    [Feat] #92 - UserRepository
    로그인 관련 유저 레포지토리 작성

---
+++ app/src/main/java/org/cazait/cazait_android/data/repository/UserRepository.kt
@@ -0,0 +1,21 @@
+package org.cazait.cazait_android.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import org.cazait.cazait_android.data.Resource
+import org.cazait.cazait_android.data.model.remote.request.LoginRequest
+import org.cazait.cazait_android.data.model.remote.request.SignUpRequest
+import org.cazait.cazait_android.data.model.remote.response.LoginResponse
+import org.cazait.cazait_android.data.model.remote.response.SignUpResponse
+
+interface UserRepository {
+
+    suspend fun signUp(body: SignUpRequest): Flow<Resource<SignUpResponse>>
+
+    suspend fun login(body: LoginRequest): Flow<Resource<LoginResponse>>
+
+    suspend fun isLoggedIn(): Flow<Boolean>
+
+    suspend fun saveToken(token: List<String>)
+
+    suspend fun getToken(): Flow<List<String>>
+}
