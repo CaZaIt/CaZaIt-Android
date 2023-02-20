826458c83f621e5f3ca0be25da97a3097e342b56
bmsk <vv99911@gmail.com>
2023-02-20 10:31:57 +0900

    [Refactor] #133 - API 수정사항 반영 및 추가

--- app/src/main/java/org/cazait/cazait_android/data/api/UserService.kt
+++ app/src/main/java/org/cazait/cazait_android/data/api/UserService.kt
@@ -1,7 +1,9 @@
 package org.cazait.cazait_android.data.api
 
+import com.google.gson.annotations.SerializedName
 import org.cazait.cazait_android.data.model.remote.request.LoginRequest
 import org.cazait.cazait_android.data.model.remote.request.SignUpRequest
+import org.cazait.cazait_android.data.model.remote.response.IsDuplicateEmailResponse
 import org.cazait.cazait_android.data.model.remote.response.LoginResponse
 import org.cazait.cazait_android.data.model.remote.response.SignUpResponse
 import org.cazait.cazait_android.data.model.remote.response.TokenResponse
@@ -9,24 +11,31 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface UserService {
-    @POST("/api/users/sign-up")
-    fun postSignUp(@Body signUpRequest: SignUpRequest): Call<SignUpResponse>
-
+    /**
+     * auths
+     */
+    
     @POST("/api/auths/log-in")
     fun postLogin(
         @Query("role") role: String = "user",
-        @Body loginRequest: LoginRequest
+        @Body loginRequest: LoginRequest,
     ): Call<LoginResponse>
-
-    @POST("/cafes/{cafeId}/favourites")
-    fun addToFavourite(@Path("cafeId") cafeId: String): Boolean
 
     @POST("/api/auths/refresh")
     fun postRefreshToken(
         @Query("role") role: String = "user",
-        @HeaderMap headers: Map<String, String>
+        @HeaderMap headers: Map<String, String>,
     ): Call<TokenResponse>
 
-    @DELETE("/cafes/{cafeId}/favourites")
-    fun removeFromFavourite(@Path("cafeId") cafeId: String): Boolean
+    /**
+     * users
+     */
+
+    @POST("/api/users/sign-up")
+    fun postSignUp(@Body signUpRequest: SignUpRequest): Call<SignUpResponse>
+
+    @GET("/api/users/email")
+    fun getIsDuplicateEmail(
+        @Query("email") email: String = "",
+    ): Call<IsDuplicateEmailResponse>
 }
