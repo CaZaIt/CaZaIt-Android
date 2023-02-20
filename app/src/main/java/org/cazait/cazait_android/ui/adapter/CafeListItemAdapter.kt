7e2f51004e37dc2348c9bd2dda82c6717011a7b4
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-02-20 19:24:59 +0900

    #102 [Feat] CafeListItemAdapter 작성
    -구글링 해보니 어댑터에서 하길래 한번 도전 해봤는데 에러 떠서 어떻게 하면 좋을지 고민 중
    - 뷰모델을 활용해야 될 꺼 같은데 능력 부족으로 못하는 중이어서 좀 더 검색해봐서 좋은 방안 강구할 예정

--- app/src/main/java/org/cazait/cazait_android/ui/adapter/CafeListItemAdapter.kt
+++ app/src/main/java/org/cazait/cazait_android/ui/adapter/CafeListItemAdapter.kt
@@ -1,18 +1,26 @@
 package org.cazait.cazait_android.ui.adapter
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import org.cazait.cazait_android.data.model.Cafe
 import org.cazait.cazait_android.databinding.ItemCafeMainBinding
 import org.cazait.cazait_android.ui.adapter.holder.CafesViewHolder
 import org.cazait.cazait_android.ui.base.RecyclerItemListener
 import org.cazait.cazait_android.ui.viewmodel.CafeListViewModel
+import java.util.logging.Filter
 
 class CafeListItemAdapter(
     private val cafeListViewModel: CafeListViewModel,
     private val cafes: List<Cafe>
-) : RecyclerView.Adapter<CafesViewHolder>() {
+) : RecyclerView.Adapter<CafesViewHolder>(), Filterable {
+
+    var filteredCafes = ArrayList<Cafe>()
+    var itemFilter = ItemFilter()
 
     private val onItemClickListener: RecyclerItemListener = object : RecyclerItemListener {
         override fun onItemSelected(cafe: Cafe) {
@@ -31,5 +39,59 @@ class CafeListItemAdapter(
 
     override fun onBindViewHolder(holder: CafesViewHolder, position: Int) {
         holder.bindForCafeListPage(cafes[position], onItemClickListener)
+    }
+
+
+    override fun getFilter(): Filter {
+        return itemFilter
+    }
+
+    fun initFilteredCafes() {
+        filteredCafes.clear()
+        filteredCafes.addAll(cafes)
+    }
+
+    inner class ItemFilter : Filter() {
+        override fun performFiltering(charSequence: CharSequence): FilterResults {
+            val filterString = charSequence.toString()
+            val results = FilterResults()
+            Log.d(TAG, "charSequence : $charSequence")
+
+            //검색이 필요 없을 경우를 위해 원본 배열을 복제
+            val filteredList: ArrayList<Cafe> = ArrayList<Cafe>()
+            //공백 제외 아무런 값이 없을 경우 -> 원본 배열
+            if (filterString.trim { it <= ' ' }.isEmpty()) {
+                results.values = cafes
+                results.count = cafes.size
+                return results
+
+                return results
+                //공백 제외 2글자 이인 경우 -> 이름으로만 검색
+            } else if (filterString.trim { it <= ' ' }.length <= 2) {
+                for (cafe in cafes) {
+                    if (cafe.name.contains(filterString)) {
+                        filteredList.add(cafe)
+                    }
+                }
+                //그 외의 경우(공백 제외 2글자 초과) -> 이름/주소로 검색
+            } else {
+                for (cafe in cafes) {
+                    if (cafe.name.contains(filterString) || cafe.address.contains(filterString)) {
+                        filteredList.add(cafe)
+                    }
+                }
+            }
+            results.values = filteredList
+            results.count = filteredList.size
+
+            return results
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults) {
+            filteredCafes.clear()
+            filteredCafes.addAll(filterResults.values as ArrayList<Cafe>)
+            notifyDataSetChanged()
+        }
     }
 }
